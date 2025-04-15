7a3ebaa5ed086f0b02d8afaf96004b50b974b770
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2025-04-15 12:18:34 +0100

    £srs

--- app.py
+++ app.py
@@ -46,8 +46,3 @@ def home():
     send_email("New Visitor Location", body)
 
     return render_template("index.html")
-
-
-if __name__ == '__main__':
-    from os import environ
-    app.run(host='0.0.0.0', port=int(environ.get("PORT", 5000)))
